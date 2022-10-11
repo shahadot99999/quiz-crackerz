f1cec4bab21e807008a533431c96aecb0d97f526
shahadot99999 <shahadot99999@gmail.com>
2022-10-11 17:11:24 +0600

    chat seting and show

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+
+    const data = [
+        {
+            name: "React",
+            total: 8,
+            pv: 2400,
+            amt: 2400
+        },
+        {
+            name: "JavaScript",
+            total: 9,
+            pv: 1398,
+            amt: 2210
+        },
+        {
+            name: "CSS",
+            total: 8,
+            pv: 9800,
+            amt: 2290
+        },
+        {
+            name: "Git",
+            total: 11,
+            pv: 3908,
+            amt: 2000
+        }
+
+    ];
+
     return (
         <div>
-            <h4>Statistics</h4>
+            <LineChart width={500} height={400} data={data}>
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
         </div>
     );
 };
